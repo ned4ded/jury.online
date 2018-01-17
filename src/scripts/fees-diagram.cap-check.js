ec0576fcb05abe70c4995139b2272deae5aee068
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-01-17 17:07:33 +0300

    call-to-action block added

--- src/scripts/fees-diagram.cap-check.js
+++ src/scripts/fees-diagram.cap-check.js
@@ -12,26 +12,3 @@
 //     }
 //   });
 // }());
-
-(function() {
-  const add = () => $('#header').addClass('header__bg');
-  const remove = () =>  $('#header').removeClass('header__bg');
-
-  $(document).ready(() => {
-    if($(window).scrollTop() <= 0) remove();
-  });
-
-  $(window).scroll((ev) => {
-    if($(window).scrollTop()) {
-      add();
-    } else {
-      remove();
-    };
-  });
-
-  $('#menu-toggler').on('click', () => {
-    if($(window).scrollTop()) return;
-    if($('#menu-toggler').attr('aria-expanded') === 'false') return add();
-    return setTimeout(() => remove(), 300);
-  });
-}());
