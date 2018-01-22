4d40ac421b2a24b672929e6bb3a0cd1c91c1440b
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-01-22 21:35:10 +0300

    added responsive slider in news-block

---
+++ src/scripts/news-block.slick-slider.js
@@ -0,0 +1,25 @@
+(function() {
+  const $slider = $('#news-block-slider');
+  const $newsCards = $slider.children();
+  const $btn = direction => $(`#news-block-slider-btn-${direction}`);
+
+  if($newsCards.length >= 4) {
+    $slider.removeClass('news-block__slider');
+    $slider.slick({
+      centerMode: true,
+      slidesToShow: 3,
+      arrows: false,
+      dots: true,
+      appendDots: $('.slick-slider'),
+      variableWidth: true,
+      dotsClass: 'news-block__slider-dots'
+    });
+
+    $btn('left').click(() => $slider.slick('slickPrev'));
+    $btn('right').click(() => $slider.slick('slickNext'));
+
+  } else {
+    $('.news-block__slider-btn').addClass('hidden-lg-up hidden-lg-down');
+    $newsCards.addClass('active');
+  }
+}());
