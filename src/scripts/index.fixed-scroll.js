6a4a67468570d1cef5322bb73e37568eefa34115
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-01-23 22:36:22 +0300

    added fixed scroll

--- src/scripts/index.fixed-scroll.js
+++ src/scripts/index.fixed-scroll.js
@@ -1,48 +1,4 @@
 // (function() {
-//   const $diagram = () => $('#fees-diagram');
-//   const wholeW = $diagram().width();
-//   const startW = (window.getComputedStyle(document.querySelector('#fees-diagram'), ':before').width).replace(/\D/g,'');
-//   const finishW = 100 - (window.getComputedStyle(document.querySelector('#fees-diagram'), ':after').width) / wholeW;
-//   const percent = Number($diagram().attr('data-complete-percent'));
-//
-//   return $(document).ready(() => {
-//     console.log(startW + " " + finishW);
-//     if(percent < (wholeW/startW)) {
-//
-//     }
-//   });
-// }());
-
-(function() {
-  const $add = () => $('#header').addClass('header__bg');
-  const $remove = () =>  $('#header').removeClass('header__bg');
-  const $togglerState = () => ($('#menu-toggler').attr('aria-expanded') === 'true');
-
-  $(document).ready(() => {
-    if($(window).scrollTop() <= 0) $remove();
-  });
-
-  $(window).scroll(() => {
-    if($(window).scrollTop()) {
-      $add();
-    } else if (!$togglerState()) {
-      $remove();
-    }
-    return;
-  });
-
-  $('#menu-toggler').on('click', () => {
-    if($(window).scrollTop()) return;
-    if(!$togglerState()) return $add();
-    return setTimeout(() => $remove(), 300);
-  });
-}());
-
-(function() {
-  $('html').removeClass('no-js');
-}());
-
-// (function() {
   var keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
   function preventDefault(e) {
@@ -166,29 +122,3 @@
     $( window ).scroll(listner);
   });
 // }());
-
-(function() {
-  const $slider = $('#news-block-slider');
-  const $newsCards = $slider.children();
-  const $btn = direction => $(`#news-block-slider-btn-${direction}`);
-
-  if($newsCards.length >= 4) {
-    $slider.removeClass('news-block__slider');
-    $slider.slick({
-      centerMode: true,
-      slidesToShow: 3,
-      arrows: false,
-      dots: true,
-      appendDots: $('.slick-slider'),
-      variableWidth: true,
-      dotsClass: 'news-block__slider-dots'
-    });
-
-    $btn('left').click(() => $slider.slick('slickPrev'));
-    $btn('right').click(() => $slider.slick('slickNext'));
-
-  } else {
-    $('.news-block__slider-btn').addClass('hidden-lg-up hidden-lg-down');
-    $newsCards.addClass('active');
-  }
-}());
