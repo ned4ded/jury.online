800d2084ba2c135ceb2ba99fd63787b187573788
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-05-18 17:43:44 +0300

    add arbiters' carousel's mobile logic

--- www/js/main.js
+++ www/js/main.js
@@ -231,199 +231,220 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 (function () {
-  var config = {
-    attr: 'data-accordion-container',
-    name: 'accordionContainer',
-    class: {
-      forward: 'arbiter-card__content--anim--forward',
-      reverse: 'arbiter-card__content--anim--reverse'
-    }
-  };
-
-  var MobileAndTabletcheck = function MobileAndTabletcheck() {
-    var check = false;
-    (function (a) {
-      if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
-    })(navigator.userAgent || navigator.vendor || window.opera);
-    return check;
-  };
-
-  var ListnerTrigger = function () {
-    function ListnerTrigger(element, state) {
-      _classCallCheck(this, ListnerTrigger);
-
-      this.element = element;
-      this.listners = [];
-      this.setState(state || false);
-      this.originalHeigh = $(this.element).height();
-    }
-
-    _createClass(ListnerTrigger, [{
-      key: 'set',
-      value: function set(name) {
-        var _this = this;
-
-        var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
-
-        var newCallback = callback.bind(this);
-
-        var fn = this.element.addEventListener(name, newCallback);
-        if (this.listners.find(function (_ref) {
-          var n = _ref.name,
-              f = _ref.fn;
-          return n === name && f === callback;
-        })) throw new Error('ListnerTrigger: such function has been already added on given event');
-
-        this.listners = [].concat(_toConsumableArray(this.listners), [{
-          name: name,
-          fn: callback,
-          removeListner: function removeListner() {
-            return _this.element.removeEventListener(name, newCallback);
-          }
-        }]);
-
-        return this;
+  $(document).ready(function () {
+    var config = {
+      attr: 'data-accordion-container',
+      name: 'accordionContainer',
+      class: {
+        forward: 'arbiter-card__content--anim--forward',
+        reverse: 'arbiter-card__content--anim--reverse'
       }
-    }, {
-      key: 'get',
-      value: function get(name) {
-        var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
+    };
 
-        return this.listners.find(function (_ref2) {
-          var n = _ref2.name,
-              cb = _ref2.fn;
-          return n === name && cb === callback;
-        });
+    var MobileAndTabletcheck = function MobileAndTabletcheck() {
+      var check = false;
+      (function (a) {
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+      })(navigator.userAgent || navigator.vendor || window.opera);
+      return check;
+    };
+
+    var ListnerTrigger = function () {
+      function ListnerTrigger(element, state) {
+        _classCallCheck(this, ListnerTrigger);
+
+        this.element = element;
+        this.listners = [];
+        this.setState(state || false);
+        this.originalHeigh = $(this.element).outerHeight();
       }
-    }, {
-      key: 'remove',
-      value: function remove(name) {
-        var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
 
-        var obj = this.get(name, callback);
-        if (!obj) return obj;
+      _createClass(ListnerTrigger, [{
+        key: 'set',
+        value: function set(name) {
+          var _this = this;
 
-        this.listners = this.listners.filter(function (o) {
-          return o !== obj;
-        });
-        obj.removeListner();
+          var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
 
-        return this;
-      }
-    }, {
-      key: 'getState',
-      value: function getState(str) {
-        switch (this.state) {
-          case true:
-            return str !== 'boolean' ? this.state : 'open';
-          case false:
-            return str !== 'boolean' ? this.state : 'close';
-        };
-      }
-    }, {
-      key: 'setState',
-      value: function setState(arg) {
-        switch (arg) {
-          case 'open':
-            this.state = true;
-            break;
-          case 'close':
-            this.state = false;
-            break;
-          default:
-            this.state = !this.state;
-            break;
+          var newCallback = callback.bind(this);
+
+          var fn = this.element.addEventListener(name, newCallback);
+          if (this.listners.find(function (_ref) {
+            var n = _ref.name,
+                f = _ref.fn;
+            return n === name && f === callback;
+          })) throw new Error('ListnerTrigger: such function has been already added on given event');
+
+          this.listners = [].concat(_toConsumableArray(this.listners), [{
+            name: name,
+            fn: callback,
+            removeListner: function removeListner() {
+              return _this.element.removeEventListener(name, newCallback);
+            }
+          }]);
+
+          return this;
         }
+      }, {
+        key: 'get',
+        value: function get(name) {
+          var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
 
-        return;
+          return this.listners.find(function (_ref2) {
+            var n = _ref2.name,
+                cb = _ref2.fn;
+            return n === name && cb === callback;
+          });
+        }
+      }, {
+        key: 'remove',
+        value: function remove(name) {
+          var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
+
+          var obj = this.get(name, callback);
+          if (!obj) return obj;
+
+          this.listners = this.listners.filter(function (o) {
+            return o !== obj;
+          });
+          obj.removeListner();
+
+          return this;
+        }
+      }, {
+        key: 'getState',
+        value: function getState(str) {
+          switch (this.state) {
+            case true:
+              return str !== 'boolean' ? this.state : 'open';
+            case false:
+              return str !== 'boolean' ? this.state : 'close';
+          };
+        }
+      }, {
+        key: 'setState',
+        value: function setState(arg) {
+          switch (arg) {
+            case 'open':
+              this.state = true;
+              break;
+            case 'close':
+              this.state = false;
+              break;
+            default:
+              this.state = !this.state;
+              break;
+          }
+
+          return;
+        }
+      }, {
+        key: 'getConfig',
+        value: function getConfig() {
+          var $el = $(this.element);
+          var paddings = $el.innerHeight() - $el.height();
+
+          var innerHeight = $el.children().get().reduce(function (acc, el) {
+            return acc + $(el).outerHeight(true);
+          }, 0);
+
+          return { containerHeight: this.originalHeigh, innerHeight: innerHeight, paddings: paddings };
+        }
+      }, {
+        key: 'open',
+        value: function open() {
+          var _getConfig = this.getConfig(),
+              inner = _getConfig.innerHeight,
+              paddings = _getConfig.paddings;
+
+          $(this.element).animate({
+            height: inner + paddings
+          });
+
+          this.setState('open');
+
+          return this;
+        }
+      }, {
+        key: 'close',
+        value: function close() {
+          var _getConfig2 = this.getConfig(),
+              containerHeight = _getConfig2.containerHeight;
+
+          $(this.element).animate({
+            height: containerHeight
+          });
+
+          this.setState('close');
+
+          return this;
+        }
+      }]);
+
+      return ListnerTrigger;
+    }();
+
+    var getContainers = function getContainers() {
+      return $('[' + config.attr + ']').get().map(function (el) {
+        var listener = new ListnerTrigger(el, $(el).data('state'));
+        return listener;
+      });
+    };
+
+    var $carousel = $('#arbiters-block-carousel');
+
+    if (!$carousel.length) return;
+
+    var slidesCount = $carousel.find('.carousel-item').length;
+    var randomizedSlide = Math.floor(Math.random() * slidesCount);
+
+    var containers = getContainers();
+
+    containers.forEach(function (container) {
+      function onMouseenter(ev) {
+        $(this.element).addClass(config.class.forward);
+        $(this.element).removeClass(config.class.reverse);
+        this.open();
+        $carousel.carousel('pause');
+      };
+
+      function onMouseleave(ev) {
+        $(this.element).addClass(config.class.reverse);
+        $(this.element).removeClass(config.class.forward);
+        this.close();
+        $carousel.carousel('cycle');
+      };
+
+      if (MobileAndTabletcheck()) {
+        var firstTouch = function firstTouch(ev) {
+          this.remove('touchstart', firstTouch);
+
+          onMouseenter.bind(this, ev)();
+          this.set('touchstart', secondTouch);
+        };
+
+        var secondTouch = function secondTouch(ev) {
+          this.remove('touchstart', secondTouch);
+
+          onMouseleave.bind(this, ev)();
+          this.set('touchstart', firstTouch);
+        };
+
+        container.set('touchstart', firstTouch);
+      } else {
+        container.set('mouseenter', onMouseenter);
+        container.set('mouseleave', onMouseleave);
       }
-    }, {
-      key: 'getConfig',
-      value: function getConfig() {
-        var $el = $(this.element);
-        var paddings = $el.innerHeight() - this.originalHeigh;
 
-        var innerHeight = $el.children().get().reduce(function (acc, el) {
-          return acc + $(el).outerHeight(true);
-        }, 0);
+      $carousel.on('slide.bs.carousel', function () {
+        container.close();
+      });
 
-        return { containerHeight: this.originalHeigh, innerHeight: innerHeight, paddings: paddings };
-      }
-    }, {
-      key: 'open',
-      value: function open() {
-        var _getConfig = this.getConfig(),
-            inner = _getConfig.innerHeight,
-            paddings = _getConfig.paddings;
-
-        $(this.element).animate({
-          height: inner + paddings
-        });
-
-        this.setState('open');
-
-        return this;
-      }
-    }, {
-      key: 'close',
-      value: function close() {
-        var _getConfig2 = this.getConfig(),
-            containerHeight = _getConfig2.containerHeight;
-
-        $(this.element).animate({
-          height: containerHeight
-        });
-
-        this.setState('close');
-
-        return this;
-      }
-    }]);
-
-    return ListnerTrigger;
-  }();
-
-  var getContainers = function getContainers() {
-    return $('[' + config.attr + ']').get().map(function (el) {
-      var listener = new ListnerTrigger(el, $(el).data('state'));
-      return listener;
+      return;
     });
-  };
 
-  var $carousel = $('#arbiters-block-carousel');
-
-  if (!$carousel.length) return;
-
-  var slidesCount = $carousel.find('.carousel-item').length;
-  var randomizedSlide = Math.floor(Math.random() * slidesCount);
-
-  var contaienrs = getContainers();
-
-  contaienrs.forEach(function (container) {
-    function onMouseenter(ev) {
-      $(this.element).addClass(config.class.forward);
-      $(this.element).removeClass(config.class.reverse);
-      this.open();
-    };
-
-    function onMouseleave(ev) {
-      $(this.element).addClass(config.class.reverse);
-      $(this.element).removeClass(config.class.forward);
-      this.close();
-    };
-
-    if (MobileAndTabletcheck()) {
-      container.set('touchstart', onMouseleave);
-      container.set('touchend', onMouseleave);
-    } else {
-      container.set('mouseenter', onMouseenter);
-      container.set('mouseleave', onMouseleave);
-    }
-
-    return;
+    return $carousel.carousel(randomizedSlide).carousel({ ride: true });
   });
-
-  return $carousel.carousel(randomizedSlide).carousel({ ride: true });
 })();
 'use strict';
 
